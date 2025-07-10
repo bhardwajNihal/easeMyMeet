7c374fd97c416e403ad598aadcbe1515f20e73cf
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2025-07-10 14:29:18 +0530

    ui fix to privacy-policy page

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+    return (
+        <div>
+            <p>
+                <b>Welcome to EaseMyMeet!</b> <br />Your privacy is important to us. This Privacy Policy explains how we collect, use, and protect your personal information when you use our services.
+
+                1. What We Collect <br/>
+                Email Address (via Google OAuth or Email Sign-In) <br/>
+
+                Basic Profile Info (name, profile picture, only if permitted by Google) <br/>
+
+                2. How We Use It <br/>
+                We use your information to: <br/>
+
+                - Authenticate you securely <br/>
+
+                - Personalize your experience <br/>
+
+                - Improve our scheduling services <br/>
+
+                3. Data Sharing <br/>
+                We do not share or sell your data with third parties. <br/>
+
+                4. Data Retention <br/>
+                We store your information as long as your account exists, and you can request deletion anytime. <br/>
+
+                5. Third-Party Services <br/>
+                We use Google for authentication. Google may collect data as described in their Privacy Policy. <br/>
+
+                6. Contact <br/>
+                If you have any questions, please contact us at: <br/>
+                    - nihalbhardwaj2112@gmail.com <br/>
+            </p>
+        </div>
+    )
+}
+
+export default PrivacyPolicy
